777163495b3dbcfb729f2d7ee163193161f91e0c
DaVinCi <Fox.winner9@gmail.com>
2023-12-18 16:49:04 +0300

    chat screen message sender

--- app/src/main/java/ru/ns/messengerlesson/ui/chat/ChatScreen.kt
+++ app/src/main/java/ru/ns/messengerlesson/ui/chat/ChatScreen.kt
@@ -1,13 +1,24 @@
 package ru.ns.messengerlesson.ui.chat
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import ru.ns.messengerlesson.data.remote.Message
 
 @Composable
 fun Message(modifier: Modifier = Modifier, message: Message) {
-
+    Text(
+        modifier = Modifier,
+        fontSize = 13.sp,
+        text = message.sender.name,
+        color = Color(0xFF0EB355),
+        fontWeight = FontWeight.Bold
+    )
 }
 
 @Preview
