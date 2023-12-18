9b1aac433d94ba0177179418e36dd7d932ea5519
Sekunov Ilya <sekunovila@gmail.com>
2023-12-18 17:55:40 +0300

    main activity with view model user cases

--- app/src/main/java/ru/ns/messengerlesson/MainActivity.kt
+++ app/src/main/java/ru/ns/messengerlesson/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.android.AndroidEntryPoint
+import ru.ns.messengerlesson.ui.register.RegisterScreen
 import ru.ns.messengerlesson.ui.theme.MessengerLessonTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +29,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChatApp()
         }
+    }
+}
+
+@Composable
+fun LoadingPage() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
+            color = Color(0xFF19D8E1),
+            modifier = Modifier
+                .size(50.dp)
+                .align(Alignment.Center)
+        )
     }
 }
 
@@ -31,6 +53,11 @@ fun ChatApp() {
                 .safeDrawingPadding(),
             color = MaterialTheme.colorScheme.background
         ) {
+            val viewModel: MainViewModel = viewModel()
+            when {
+                viewModel.isUserFetching -> LoadingPage()
+                viewModel.user == null -> RegisterScreen(onSubmitUsername = viewModel::saveUser)
+            }
         }
     }
 }
