16414804d6e93e213f07ccc50d64e1eeaf260cad
DaVinCi <Fox.winner9@gmail.com>
2023-12-18 16:40:56 +0300

    register screen column

--- app/src/main/java/ru/ns/messengerlesson/ui/register/RegisterScreen.kt
+++ app/src/main/java/ru/ns/messengerlesson/ui/register/RegisterScreen.kt
@@ -1,6 +1,9 @@
 package ru.ns.messengerlesson.ui.register
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
@@ -12,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,24 +25,30 @@ import androidx.compose.ui.unit.dp
 fun RegisterScreen(
     modifier: Modifier = Modifier,
     onSubmitUsername: (String) -> Unit
-)  {
-    var username by remember { mutableStateOf("") }
-    TextField(
-        modifier = Modifier.fillMaxWidth(0.5f),
-        value = username,
-        onValueChange = { username = it },
-        placeholder = { Text(text = "Enter username") }
-    )
-    Spacer(modifier = Modifier.height(10.dp))
-    Button(
-        modifier = Modifier.fillMaxWidth(0.5f),
-        onClick = {
-            if (username.length >= 4) {
-                onSubmitUsername(username)
-            }
-        }
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Enter a chat")
+        var username by remember { mutableStateOf("") }
+        TextField(
+            modifier = Modifier.fillMaxWidth(0.5f),
+            value = username,
+            onValueChange = { username = it },
+            placeholder = { Text(text = "Enter username") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth(0.5f),
+            onClick = {
+                if (username.length >= 4) {
+                    onSubmitUsername(username)
+                }
+            }
+        ) {
+            Text(text = "Enter a chat")
+        }
     }
 }
 
