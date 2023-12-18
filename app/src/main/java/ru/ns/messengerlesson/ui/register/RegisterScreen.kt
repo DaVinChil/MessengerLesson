4cba4f37aa88b71b7012b8e42a781232270e10ab
DaVinCi <Fox.winner9@gmail.com>
2023-12-18 16:36:14 +0300

    register screen start

---
+++ app/src/main/java/ru/ns/messengerlesson/ui/register/RegisterScreen.kt
@@ -0,0 +1,15 @@
+package ru.ns.messengerlesson.ui.register
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegisterScreen() {
+
+}
+
+@Preview
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen()
+}
