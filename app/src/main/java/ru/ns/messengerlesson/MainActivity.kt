d3f6f64a58f7835004821e1800901019dea14d1c
Sekunov Ilya <sekunovila@gmail.com>
2023-12-18 17:49:06 +0300

    main activity start

--- app/src/main/java/ru/ns/messengerlesson/MainActivity.kt
+++ app/src/main/java/ru/ns/messengerlesson/MainActivity.kt
@@ -3,28 +3,34 @@ package ru.ns.messengerlesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.ns.messengerlesson.ui.theme.MessengerLessonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            MessengerLessonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            ChatApp()
+        }
+    }
+}
 
-                }
-            }
+@Composable
+fun ChatApp() {
+    MessengerLessonTheme {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .safeDrawingPadding(),
+            color = MaterialTheme.colorScheme.background
+        ) {
         }
     }
 }
