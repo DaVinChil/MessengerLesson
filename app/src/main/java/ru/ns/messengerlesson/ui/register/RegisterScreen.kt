05d593fc451c0e0bcf0b62ef2ab9b080b4c59df3
DaVinCi <Fox.winner9@gmail.com>
2023-12-18 16:37:04 +0300

    register screen text field

--- app/src/main/java/ru/ns/messengerlesson/ui/register/RegisterScreen.kt
+++ app/src/main/java/ru/ns/messengerlesson/ui/register/RegisterScreen.kt
@@ -1,11 +1,27 @@
 package ru.ns.messengerlesson.ui.register
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreen() {
-
+    var username by remember { mutableStateOf("") }
+    TextField(
+        modifier = Modifier.fillMaxWidth(0.5f),
+        value = username,
+        onValueChange = { username = it },
+        placeholder = { Text(text = "Enter username") }
+    )
 }
 
 @Preview
