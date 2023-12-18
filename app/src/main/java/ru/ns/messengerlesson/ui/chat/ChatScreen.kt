4faae9cc790bc2835fda98a111e37d269c332d70
DaVinCi <Fox.winner9@gmail.com>
2023-12-18 16:45:05 +0300

    chat screen message start

---
+++ app/src/main/java/ru/ns/messengerlesson/ui/chat/ChatScreen.kt
@@ -0,0 +1,17 @@
+package ru.ns.messengerlesson.ui.chat
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.ns.messengerlesson.data.remote.Message
+
+@Composable
+fun Message(modifier: Modifier = Modifier, message: Message) {
+
+}
+
+@Preview
+@Composable
+fun MessagePreview() {
+
+}
