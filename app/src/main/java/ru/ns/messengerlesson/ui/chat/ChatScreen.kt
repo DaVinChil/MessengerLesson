98b389990921c0990cbb93aaf8be5202179421ff
Sekunov Ilya <sekunovila@gmail.com>
2023-12-18 18:12:28 +0300

    chat screen last touches

--- app/src/main/java/ru/ns/messengerlesson/ui/chat/ChatScreen.kt
+++ app/src/main/java/ru/ns/messengerlesson/ui/chat/ChatScreen.kt
@@ -1,6 +1,9 @@
 package ru.ns.messengerlesson.ui.chat
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,10 +15,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imeNestedScroll
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -27,9 +34,11 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,6 +48,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 import ru.ns.messengerlesson.data.remote.Message
 import ru.ns.messengerlesson.data.remote.UserDto
 import java.text.SimpleDateFormat
@@ -66,23 +76,51 @@ fun ChatContent(
     isMineMessage: (Message) -> Boolean,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        contentPadding = PaddingValues(10.dp),
-        verticalArrangement = Arrangement.spacedBy(5.dp),
-        modifier = modifier
-    ) {
-        items(
-            count = messages.size,
-            key = { messages[it].id }
+    val chatListState = rememberLazyListState(
+        initialFirstVisibleItemIndex = messages.lastIndex
+    )
+    val showScrollToBottom by remember {
+        derivedStateOf {
+            chatListState.isNotScrolledToTheEnd()
+        }
+    }
+    val coroutineScope = rememberCoroutineScope()
+    Box(modifier = modifier) {
+        LazyColumn(
+            state = chatListState,
+            contentPadding = PaddingValues(10.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
         ) {
-            val arrangement =
-                if (isMineMessage(messages[it])) Arrangement.End else Arrangement.Start
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = arrangement
+            items(
+                count = messages.size,
+                key = { messages[it].id }
             ) {
-                Message(message = messages[it])
+                val arrangement =
+                    if (isMineMessage(messages[it])) Arrangement.End else Arrangement.Start
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = arrangement
+                ) {
+                    Message(message = messages[it])
+                }
             }
+        }
+        AnimatedVisibility(
+            visible = showScrollToBottom,
+            label = "animated scroll button",
+            enter = fadeIn(initialAlpha = 0.1f),
+            exit = fadeOut(),
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            ScrollDownButton(
+                onClick = {
+                    coroutineScope.launch {
+                        chatListState.animateScrollToItem(messages.lastIndex)
+                    }
+                },
+                itemsCountToScrollThrough = chatListState.itemsCountAfterLastVisibleItem(),
+                modifier = Modifier.padding(20.dp)
+            )
         }
     }
 }
@@ -193,6 +231,14 @@ fun ScrollDownButton(
 
 @SuppressLint("SimpleDateFormat")
 fun formatDate(date: Date): String = SimpleDateFormat("hh:mm").format(date)
+
+fun LazyListState.itemsCountAfterLastVisibleItem(): Int {
+    val lastVisibleItemIndex = layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: -1
+    val lastItemIndex = layoutInfo.totalItemsCount - 1
+    return lastItemIndex - lastVisibleItemIndex
+}
+
+fun LazyListState.isNotScrolledToTheEnd() = itemsCountAfterLastVisibleItem() > 0
 
 @Preview
 @Composable
