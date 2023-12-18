d18a76e37a0ea45cc811adc938c58d3e0e7017cc
DaVinCi <Fox.winner9@gmail.com>
2023-12-18 16:52:45 +0300

    chat screen message message and date

--- app/src/main/java/ru/ns/messengerlesson/ui/chat/ChatScreen.kt
+++ app/src/main/java/ru/ns/messengerlesson/ui/chat/ChatScreen.kt
@@ -1,5 +1,7 @@
 package ru.ns.messengerlesson.ui.chat
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -7,8 +9,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.ns.messengerlesson.data.remote.Message
+import ru.ns.messengerlesson.data.remote.UserDto
+import java.util.Date
 
 @Composable
 fun Message(modifier: Modifier = Modifier, message: Message) {
@@ -19,10 +24,28 @@ fun Message(modifier: Modifier = Modifier, message: Message) {
         color = Color(0xFF0EB355),
         fontWeight = FontWeight.Bold
     )
+    Text(
+        modifier = Modifier,
+        fontSize = 17.sp,
+        text = message.message
+    )
+    Spacer(modifier = Modifier.width(5.dp))
+    Text(
+        modifier = Modifier,
+        fontSize = 10.sp,
+        text = message.date.toString()
+    )
 }
 
 @Preview
 @Composable
 fun MessagePreview() {
-
+    Message(
+        message = Message(
+            id = 1L,
+            sender = UserDto("Tom"),
+            message = "Hello",
+            date = Date()
+        )
+    )
 }
